d88e7ffe50d3f837f95791a5c3bafa66b95fe70a
Григорий Козлов <g.kozlov@fairtech.ru>
2022-01-07 16:04:48 +0300

    init control via numpad

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,23 @@
 #include <Wire.h>                   // for I2C communication
 #include <LiquidCrystal_I2C.h>      // for LCD
 #include <RTClib.h>                 // for RTC
+#include <Keypad.h>                 // for controll from keypad
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // create LCD with I2C address 0x27, 16 characters per line, 2 lines
 RTC_DS3231 rtc;                     // create rtc for the DS3231 RTC module, address is fixed at 0x68
 
+const byte ROWS = 4;
+const byte COLS = 4;
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {9, 8, 7, 6};
+byte colPins[COLS] = {5, 4, 3, 2};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 /*
    function to update RTC time using user input
 */
@@ -140,5 +153,9 @@ void loop()
   if (Serial.available()) {
     char input = Serial.read();
     if (input == 'u') updateRTC();  // update RTC time
+  }
+  char key = keypad.getKey();
+  if (key){
+    Serial.println(key);
   }
 }
